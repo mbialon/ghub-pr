377d5ee946def0b0511ac1b603337f234886a289
Marcin Białoń <marcin@bialon.net>
2019-02-26 23:25:18 +0100

    Select a pull-request

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/mbialon/ghub-pr
+
+go 1.12
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/manifoldco/promptui v0.3.2
+	github.com/nicksnyder/go-i18n v1.10.0 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/shurcooL/githubv4 v0.0.0-20190119021625-d9689b595017
+	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
+	golang.org/x/net v0.0.0-20190226193003-66a96c8a540e // indirect
+	golang.org/x/oauth2 v0.0.0-20190226191147-529b322ea346
+	gopkg.in/alecthomas/kingpin.v3-unstable v3.0.0-20180810215634-df19058c872c // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
+type PullRequest struct {
+	Number      int
+	Title       string
+	BaseRefName string
+	HeadRefName string
+	Closed      bool
+}
+
+func main() {
+	owner := flag.String("owner", "", "github repository owner")
+	name := flag.String("name", "", "github repository name")
+	all := flag.Bool("all", false, "list both open and closed")
+	flag.Parse()
+
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: os.Getenv("GITHUB_TOKEN"),
+		},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
+
+	var q struct {
+		Repository struct {
+			PullRequests struct {
+				Nodes    []PullRequest
+				PageInfo struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+			} `graphql:"pullRequests(first: 100, after: $pullRequestsCursor)"`
+		} `graphql:"repository(owner: $repositoryOwner, name: $repositoryName)"`
+	}
+	vars := map[string]interface{}{
+		"repositoryOwner":    githubv4.String(*owner),
+		"repositoryName":     githubv4.String(*name),
+		"pullRequestsCursor": (*githubv4.String)(nil),
+	}
+	var prs []PullRequest
+	for {
+		err := client.Query(context.Background(), &q, vars)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, pr := range q.Repository.PullRequests.Nodes {
+			if pr.Closed && !*all {
+				continue
+			}
+			prs = append(prs, pr)
+		}
+		if !q.Repository.PullRequests.PageInfo.HasNextPage {
+			break
+		}
+		vars["pullRequestsCursor"] = githubv4.NewString(q.Repository.PullRequests.PageInfo.EndCursor)
+	}
+
+	templates := &promptui.SelectTemplates{
+		Active:   "> {{ .Title }}",
+		Inactive: "  {{ .Title }}",
+		Selected: "{{ .Number }} {{ .HeadRefName }}",
+	}
+
+	searcher := func(input string, index int) bool {
+		pr := prs[index]
+		name := strings.Replace(strings.ToLower(pr.Title), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	s := promptui.Select{
+		Label:     "Select a pull-request",
+		Items:     prs,
+		Templates: templates,
+		Size:      4,
+		Searcher:  searcher,
+	}
+	_, _, err := s.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
